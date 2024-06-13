f52525c6da4877b458bd4cbe17e5a7834e7a2d75
Sakila-lama <shakila.lama@gmail.com>
2024-06-13 00:20:36 -0400

    edited route/index.js

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+/* GET about page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+/* GET project page */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+/* GET contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
