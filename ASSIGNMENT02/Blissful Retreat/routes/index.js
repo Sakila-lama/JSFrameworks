449ad3a905c039717bae968d1af80934f66a6ea6
Sakila-lama <shakila.lama@gmail.com>
2024-08-08 20:52:10 -0400

    added routes in Express.js

--- ASSIGNMENT02/Blissful Retreat/routes/index.js
+++ ASSIGNMENT02/Blissful Retreat/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Main home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+// Booking page route
+router.get('/book', function(req, res, next) {
+  res.render('book', { title: 'Book Online' });
+});
+
+// Login page route
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Log In' });
+});
+
+// Register page route (Optional: can be combined with login if desired)
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
